3ca6e1d557da629f2babb2aeb9c2ca9600956762
abiinnovations <infoabiinnovations@gmail.com>
2021-03-18 17:10:20 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/Todo'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header/>         
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
+ 
